92fd49020482c349cd34403cdbf8ea8977fcff1c
Sumit singh <61839226+sumitsingh0003@users.noreply.github.com>
2026-03-09 15:17:48 +0530

    Change socket connection URL to production

--- services/socket.js
+++ services/socket.js
@@ -1,5 +1,8 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:5000",{
-  transports:["websocket"]
-});
+export const socket = io(
+  "https://kanban-board-api-zsyw.onrender.com",
+  {
+    transports:["websocket"]
+  }
+);
